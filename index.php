268ef935d5037ac5ae9dc048a263016779babaca
Gus <yo@soygus.com>
2015-05-15 18:39:50 -0500

    Init

--- index.php
+++ index.php
@@ -1,35 +1,17 @@
-<?php get_header(); ?>
-	
-		<main role="main" class="site-content">
-			<div class="site-content-left"></div>
-			<div class="site-content-right">
-				<div class="excerpt-wrap">
-					<h1 class="lead"><strong>I'm a design-oriented fullstack JavaScript developer.</strong> I like to focus on: digital innovation, education, linguistics, AI, IoT and audio interfaces. If I don't know the tools you need for your project, <strong>I'm an excellent learner.</strong></h1>
-				</div>
-				<div class="section-title">
-					<div class="section-title-left">
-						<h5>Latest posts</h5>
-					</div>
-					<nav class="section-title-right">
-						<a href="#">See more</a>
-					</nav>
-				</div>
-				<section class="posts-list">
-					<?php $query = new WP_Query('showposts=3') ?>
-					<?php while( $query->have_posts() ) : $query->the_post(); ?>
-						<article class="posts-list-item">
-							<div class="posts-list-item-left">
-								<div class="post-title"><h2><?php the_title(); ?></h2></div>
-								<nav class="post-meta"><?php the_category(' '); ?></nav>
-							</div>
-							<div class="posts-list-item-right">
-								<p><?php the_excerpt(); ?><a href="<?php the_permalink(); ?>">read more</a></p>
-							</div>
-						</article>
-					<?php endwhile; ?>
-					<?php wp_reset_query(); ?>
-				</section>
-			</div>
-		</main>
-		
-<?php get_footer(); ?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
